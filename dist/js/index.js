ed72bee7b847f77c482d4cc678fbdcd78bf7246d
VsevolodX <vsevolodbiryukov@icloud.com>
2025-11-14 12:11:38 -0800

    chore: commit dist

---
+++ dist/js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "ComputedEntityMixin", {
+  enumerable: true,
+  get: function () {
+    return _compute.ComputedEntityMixin;
+  }
+});
+Object.defineProperty(exports, "getDefaultComputeConfig", {
+  enumerable: true,
+  get: function () {
+    return _default.getDefaultComputeConfig;
+  }
+});
+
+var _compute = require("./compute");
+
+var _default = require("./default");

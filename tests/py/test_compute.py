5cee8f7cb252ec229e141aa315eda1eea1624e6d
VsevolodX <vsevolodbiryukov@icloud.com>
2026-02-18 18:28:55 -0800

    update: test ide class

---
+++ tests/py/test_compute.py
@@ -0,0 +1,20 @@
+from mat3ra.ide.compute import ComputeConfiguration
+from mat3ra.ide.compute import Cluster, Queue
+
+
+def test_compute_configuration():
+    """Test ComputeConfiguration can be created with Cluster."""
+    cluster = Cluster(fqdn="master-1-staging.exabyte.io")
+    compute = ComputeConfiguration(
+        queue=Queue.D,
+        nodes=1,
+        ppn=1,
+        cluster=cluster,
+    )
+    assert compute.queue == Queue.OR8
+    assert compute.queue == "OR8"
+    assert compute.nodes == 1
+    assert compute.ppn == 8
+    assert compute.cluster is not None
+    assert compute.cluster.fqdn == "master-1-staging.exabyte.io"
+    assert compute.cluster.jid is None

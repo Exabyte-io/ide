91d3accc7d6cae6b0d1d9c6252d96f56c4abc193
VsevolodX <vsevolodbiryukov@icloud.com>
2026-02-18 18:39:08 -0800

    update: let the tests run

--- tests/py/test_compute.py
+++ tests/py/test_compute.py
@@ -3,16 +3,14 @@ from mat3ra.ide import Cluster, Queue
 
 
 def test_compute_configuration():
-    """Test ComputeConfiguration can be created with Cluster."""
     cluster = Cluster(fqdn="master-1-staging.exabyte.io")
     compute = ComputeConfiguration(
         queue=Queue.D,
         nodes=1,
-        ppn=1,
+        ppn=8,
         cluster=cluster,
     )
-    assert compute.queue == Queue.OR8
-    assert compute.queue == "OR8"
+    assert compute.queue == Queue.D
     assert compute.nodes == 1
     assert compute.ppn == 8
     assert compute.cluster is not None

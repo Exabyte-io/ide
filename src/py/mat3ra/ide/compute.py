b24d453a5466de2c49a2056deb6204367989d87c
VsevolodX <vsevolodbiryukov@icloud.com>
2026-02-18 18:34:17 -0800

    update: use esse for compute

--- src/py/mat3ra/ide/compute.py
+++ src/py/mat3ra/ide/compute.py
@@ -1,21 +1,18 @@
 from typing import Optional
 
 from mat3ra.code.entity import InMemoryEntityPydantic
-from mat3ra.esse.models.job.compute import Cluster as ClusterESSE, Queue
+from mat3ra.esse.models.job.compute import Cluster as ClusterESSE, Queue, ComputeArgumentsSchema
+
 
 class Cluster(InMemoryEntityPydantic, ClusterESSE):
     pass
 
 
-class ComputeConfiguration(InMemoryEntityPydantic):
+class ComputeConfiguration(InMemoryEntityPydantic, ComputeArgumentsSchema):
     queue: Queue
     nodes: int = 1
     ppn: int = 1
     cluster: Optional[Cluster] = None
-    timeLimit: Optional[str] = None
-    notify: Optional[str] = None
-    timeLimitType: Optional[str] = None
-    isRestartable: Optional[bool] = None
-    email: Optional[str] = None
+    # This should be defined based on the queue (or cluster) used
     maxCPU: Optional[int] = None
 
